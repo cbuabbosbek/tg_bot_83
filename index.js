7b8ea2142c0feb7db34079aff2a35a06c431b1ab
abbossth <a@test.ru>
2025-10-25 15:52:47 +0500

    telegram bot tayyor

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+import TelegramBot from "node-telegram-bot-api";
+
+const TOKEN = "8399278388:AAGykqnSENSW30QNMGLJYFUbG-5-JskSSuE";
+
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+bot.on("message", function (msg) {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+
+  bot.sendMessage(chatId, `Salom -> ${text}`);
+
+  console.log(msg);
+  //   console.log("*********");
+  //   console.log(chatId, text);
+});
+
 console.log("Bot ishga tushdi...");
